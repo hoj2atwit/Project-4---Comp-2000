46a8c3a94af19e324a87d6d0e5ed535a9b055202
hoj2 <hoj2@WIT45001220.wit.private>
2019-10-18 11:43:35 -0400

    bah

---
+++ Project 4 - Card Game - Comp 2000/src/CardGame.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class CardGame {
+	
+	private ArrayList<Player> players;
+	private Deck deck;
+	
+	public static void main(String[] args){
+		initialize();
+		
+		play();
+	}
+	
+	public static void initialize() {
+		
+	}
+	
+	public static void play() {
+		
+	}
+	
+	
+}
